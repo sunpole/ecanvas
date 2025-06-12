7eda7b0522c072ea6aa1cfc022dd6cba4af76c9b
ANTON MAHOMEDOV <akitsatnafiya@gmail.com>
2025-06-12 21:00:48 +0300

    Update spawner.js

--- scripts/spawner.js
+++ scripts/spawner.js
@@ -1,1 +1,35 @@
+// scripts/spawner.js
 
+/**
+ * Запуск новой волны заказов
+ */
+export function startWave(waveData) {
+  console.log('[SPAWNER] startWave:', waveData);
+  // TODO: инициализировать очередь заказов, сбросить состояние спавнера
+}
+
+/**
+ * Формирование очереди заказов на волну
+ */
+export function queueOrders(waveData) {
+  console.log('[SPAWNER] queueOrders:', waveData);
+  // TODO: подготовить массив order'ов для данной волны
+  return [];
+}
+
+/**
+ * Таймер — регулярный спаун заказов по очереди
+ */
+export function tickSpawner() {
+  console.log('[SPAWNER] tickSpawner');
+  // TODO: раз в N миллисекунд создавать новый заказ из очереди, пока не закончится волна
+}
+
+/**
+ * Проверка окончания волны
+ */
+export function isWaveFinished() {
+  console.log('[SPAWNER] isWaveFinished');
+  // TODO: если заказов не осталось — вернуть true, иначе false
+  return true;
+}
