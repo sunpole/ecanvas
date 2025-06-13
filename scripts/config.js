ef146d0ac6c841319bf93d72c43a2427f8b94516
ANTON MAHOMEDOV <akitsatnafiya@gmail.com>
2025-06-13 06:45:19 +0300

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -5,10 +5,11 @@ export const OUTLINE = 1;
 export const GRID_TOTAL = GRID_SIZE + 2 * OUTLINE;
 
 export const SPAWN_CELLS = [
-  { row: Math.floor(GRID_SIZE/2)-1, col: 0 },
-  { row: Math.floor(GRID_SIZE/2),   col: 0 },
+  { row: Math.floor(GRID_SIZE / 2) - 1 + OUTLINE, col: 0 },
+  { row: Math.floor(GRID_SIZE / 2) + OUTLINE, col: 0 },
 ];
+
 export const EXIT_CELLS = [
-  { row: Math.floor(GRID_SIZE/2)-1, col: GRID_SIZE + OUTLINE },
-  { row: Math.floor(GRID_SIZE/2),   col: GRID_SIZE + OUTLINE },
+  { row: Math.floor(GRID_SIZE / 2) - 1 + OUTLINE, col: GRID_SIZE + OUTLINE },
+  { row: Math.floor(GRID_SIZE / 2) + OUTLINE, col: GRID_SIZE + OUTLINE },
 ];
